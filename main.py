7a1ee4f285fc1ff5ff269a2141d5bb91250e5a25
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-07-10 20:52:54 +0200

    rock paper scissors game made

--- main.py
+++ main.py
@@ -22,3 +22,31 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
     return False
+
+def play_best_of(n):
+     # play against the computer until someone wins best of n games
+    # to win, you must win ceil(n/2) games (ie 2/3, 3/5, 4/7)
+    player_wins = 0
+    computer_wins = 0
+    wins_necessary = math.ceil(n/2)
+    while player_wins < wins_necessary and computer_wins < wins_necessary:
+        result, user,computer = play()
+        #tie
+        if result == 0:
+            print('Its a tie. You and the computer have both chosen{}.\n'.format(user))
+       
+        # you win
+        elif result == 1:
+            player_wins += 1
+            print('You chose {} and the computer chose {}. You won!\n'.format(user, computer))    
+        else:
+            computer_wins +=1
+            print('you chose {} and the computer chose {}. you lost :(\n'.format(user, computer))
+    if player_wins > computer_wins:
+        print('You have won the best of {} games! What a champ :D'.format(n))
+    else:
+        print('Unfortunately, the computer has won the best of {} games. Better luck next time!'.format(n))
+
+
+if __name__ == '__main__':
+    play_best_of(3) # 2
